c37329129752bc04bdb356eb508687155d560268
DasunMax970 <dasunmax08@gmail.com>
2024-08-13 05:51:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/900435c6d3157c98c3c88.jpg",
+ALIVE_MSG:process.env.ALIVE_MSG || "Hello I am ANJANA-MD Iam alive now"
 };
