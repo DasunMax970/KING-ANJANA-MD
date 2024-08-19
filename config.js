36fb78f3dd76a4a63d957307d60210dfdba2f6e0
DasunMax970 <dasunmax08@gmail.com>
2024-08-19 19:52:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,30 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
  ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/900435c6d3157c98c3c88.jpg",
-ALIVE_MSG:process.env.ALIVE_MSG || "Hello I am ANJANA-MD Iam alive now"
+ALIVE_MSG:process.env.ALIVE_MSG || "𝙰𝙽𝚈𝚃𝙷𝙸𝙽𝙶 𝙵𝙾𝚁 𝚈𝙾𝚄
+
+
+             🐼 ... 𝚂𝚄𝙳𝙳𝙰 ...🐼
+
+
+              𝙸 𝙰𝙼 𝙼𝚄𝙻𝚃𝙸 𝙳𝙴𝚅𝙸𝙲𝙴
+            ©️ 𝚆𝚃𝚂 𝙱𝙾𝚃  
+
+
+🐼 𝙾𝚆𝙽𝙴𝙳 𝙱𝚈 : ANJANA MD AND DASUN MAX
+
+
+ 𝙾𝚆𝙽𝙴𝚁𝚂:+94760105256,+94765775550
+
+
+
+                   
+                  
+
+
+    
+
+
+           ᴾᴼᵂᴱᴿᴰ ᴮʸ
+   ᴋɪɴɢ-ᴀɴᴊᴀɴᴀ :ввн׀🍁👻💦"
 };
